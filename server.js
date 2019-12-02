30a2203e969137ddc184ec1633cee96a9d647049
Marc Lee <chlee1252@gmail.com>
2019-12-01 17:17:52 -0800

    Get RestApi Customer info

--- server.js
+++ server.js
@@ -6,8 +6,33 @@ const port = process.env.PORT || 5000;
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true}));
 
-app.get('/api/hello', (req, res) => {
-    res.send({message: 'Hello Express!'});
-});
+app.get('/api/customers', (req, res) => {
+    res.send([
+        {
+          'id': 1,
+          'image': 'https://placeimg.com/64/64/1',
+          'name': 'Changhwan Lee',
+          'birthday': '951127',
+          'gender': 'male',
+          'job': 'student'
+        },
+        {
+          'id': 2,
+          'image': 'https://placeimg.com/64/64/2',
+          'name': 'Hello',
+          'birthday': '951127',
+          'gender': 'male',
+          'job': 'word'
+        },
+        {
+          'id': 3,
+          'image': 'https://placeimg.com/64/64/3',
+          'name': 'World',
+          'birthday': '951127',
+          'gender': 'male',
+          'job': 'word'
+        }]
+    )
+})
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
